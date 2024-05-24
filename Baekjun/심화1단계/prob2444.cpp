4e78cd4f5dddcafc0c047f2386a1165bef1d7fdb
Hyeonos <79961865+hyeonos@users.noreply.github.com>
2024-05-24 15:50:26 +0900

    Create prob2444.cpp
    feat: added cppfile problem#2444 of Baekjun

---
+++ Baekjun/심화1단계/prob2444.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	short n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = n - i; j > 0; j--) {
+			cout << " ";
+		}
+		for (int j = 0; j < 2 * i - 1; j++) {
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+
+	for (int i = n - 1; i >= 1; i--) {
+		for (int j = n - i; j > 0; j--) {
+			cout << " ";
+		}
+		for (int j = 0; j < 2 * i - 1; j++) {
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+
+	return 0;
+}
